c21be93126fc1775fc15b7846ea8f3f11c9019f4
Kevin <kevinobedmm@gmail.com>
2025-05-13 12:56:45 -0600

    Dylan

--- ProyectoPrograI-C#/ProyectoPrograI-C#/Form1.Designer.cs
+++ ProyectoPrograI-C#/ProyectoPrograI-C#/Form1.Designer.cs
@@ -37,9 +37,10 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(108, 185);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(591, 54);
+            this.label1.Size = new System.Drawing.Size(459, 54);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Ignorar este form por ahora";
+            this.label1.Text = "Ignorar este form por";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
