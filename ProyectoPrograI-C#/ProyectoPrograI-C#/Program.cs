582227a86eb67f2bee5a6aaa6760924a525fd745
Kevin <kevinobedmm@gmail.com>
2025-05-16 00:44:25 -0600

    Cambios

--- ProyectoPrograI-C#/ProyectoPrograI-C#/Program.cs
+++ ProyectoPrograI-C#/ProyectoPrograI-C#/Program.cs
@@ -18,7 +18,6 @@ namespace ProyectoPrograI_C_
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frm_Habitaciones());
-
         }
     }
 }
