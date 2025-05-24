0fefaa91e1411125ebc9fccaec6c4c98479afda9
Kevin <kevinobedmm@gmail.com>
2025-05-24 00:38:21 -0600

    Arreglé los conflictos con el merge accidental

--- ProyectoPrograI-C#/ProyectoPrograI-C#/Presentacion/frm_Medicamentos.Designer.cs
+++ ProyectoPrograI-C#/ProyectoPrograI-C#/Presentacion/frm_Medicamentos.Designer.cs
@@ -80,13 +80,8 @@
             this.btn_Cerrar.IconColor = System.Drawing.SystemColors.GrayText;
             this.btn_Cerrar.IconFont = FontAwesome.Sharp.IconFont.Solid;
             this.btn_Cerrar.IconSize = 25;
-<<<<<<< HEAD
-            this.btn_Cerrar.Location = new System.Drawing.Point(1132, 16);
-            this.btn_Cerrar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-=======
             this.btn_Cerrar.Location = new System.Drawing.Point(1206, 13);
             this.btn_Cerrar.Margin = new System.Windows.Forms.Padding(4);
->>>>>>> dbb6ac10ba5e620f54dd412835a038d38da01451
             this.btn_Cerrar.Name = "btn_Cerrar";
             this.btn_Cerrar.Size = new System.Drawing.Size(32, 39);
             this.btn_Cerrar.TabIndex = 20;
@@ -105,13 +100,8 @@
             this.btn_Disminuir.IconColor = System.Drawing.SystemColors.GrayText;
             this.btn_Disminuir.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.btn_Disminuir.IconSize = 25;
-<<<<<<< HEAD
-            this.btn_Disminuir.Location = new System.Drawing.Point(1091, 16);
-            this.btn_Disminuir.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-=======
             this.btn_Disminuir.Location = new System.Drawing.Point(1164, 13);
             this.btn_Disminuir.Margin = new System.Windows.Forms.Padding(4);
->>>>>>> dbb6ac10ba5e620f54dd412835a038d38da01451
             this.btn_Disminuir.Name = "btn_Disminuir";
             this.btn_Disminuir.Size = new System.Drawing.Size(32, 39);
             this.btn_Disminuir.TabIndex = 19;
@@ -126,11 +116,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.GrayText;
-<<<<<<< HEAD
-            this.label1.Location = new System.Drawing.Point(659, 16);
-=======
             this.label1.Location = new System.Drawing.Point(672, 13);
->>>>>>> dbb6ac10ba5e620f54dd412835a038d38da01451
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(277, 37);
@@ -152,17 +138,10 @@
             "Tableta",
             "Aerosol",
             "Inyeccion"});
-<<<<<<< HEAD
-            this.cbx_Tipo_Medicamento.Location = new System.Drawing.Point(666, 82);
-            this.cbx_Tipo_Medicamento.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.cbx_Tipo_Medicamento.Name = "cbx_Tipo_Medicamento";
-            this.cbx_Tipo_Medicamento.Size = new System.Drawing.Size(448, 28);
-=======
             this.cbx_Tipo_Medicamento.Location = new System.Drawing.Point(545, 149);
             this.cbx_Tipo_Medicamento.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cbx_Tipo_Medicamento.Name = "cbx_Tipo_Medicamento";
             this.cbx_Tipo_Medicamento.Size = new System.Drawing.Size(277, 24);
->>>>>>> dbb6ac10ba5e620f54dd412835a038d38da01451
             this.cbx_Tipo_Medicamento.TabIndex = 21;
             this.cbx_Tipo_Medicamento.SelectedIndexChanged += new System.EventHandler(this.cbx_Tipo_Medicamento_SelectedIndexChanged);
             // 
@@ -174,17 +153,10 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.GrayText;
-<<<<<<< HEAD
-            this.label2.Location = new System.Drawing.Point(435, 88);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(229, 25);
-=======
             this.label2.Location = new System.Drawing.Point(366, 153);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(151, 20);
->>>>>>> dbb6ac10ba5e620f54dd412835a038d38da01451
             this.label2.TabIndex = 22;
             this.label2.Text = "Tipo Medicamento:";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -197,17 +169,10 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.GrayText;
-<<<<<<< HEAD
-            this.label3.Location = new System.Drawing.Point(435, 131);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(92, 25);
-=======
             this.label3.Location = new System.Drawing.Point(366, 199);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(58, 20);
->>>>>>> dbb6ac10ba5e620f54dd412835a038d38da01451
             this.label3.TabIndex = 23;
             this.label3.Text = "Costo:";
             // 
@@ -220,17 +185,10 @@
             this.txt_Costo_Medicamentos.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txt_Costo_Medicamentos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txt_Costo_Medicamentos.ForeColor = System.Drawing.SystemColors.GrayText;
-<<<<<<< HEAD
-            this.txt_Costo_Medicamentos.Location = new System.Drawing.Point(666, 131);
-            this.txt_Costo_Medicamentos.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.txt_Costo_Medicamentos.Name = "txt_Costo_Medicamentos";
-            this.txt_Costo_Medicamentos.Size = new System.Drawing.Size(448, 30);
-=======
             this.txt_Costo_Medicamentos.Location = new System.Drawing.Point(545, 193);
             this.txt_Costo_Medicamentos.Margin = new System.Windows.Forms.Padding(4);
             this.txt_Costo_Medicamentos.Name = "txt_Costo_Medicamentos";
             this.txt_Costo_Medicamentos.Size = new System.Drawing.Size(277, 19);
->>>>>>> dbb6ac10ba5e620f54dd412835a038d38da01451
             this.txt_Costo_Medicamentos.TabIndex = 24;
             this.txt_Costo_Medicamentos.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -242,17 +200,10 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.GrayText;
-<<<<<<< HEAD
-            this.label4.Location = new System.Drawing.Point(435, 186);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(239, 25);
-=======
             this.label4.Location = new System.Drawing.Point(366, 287);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(157, 20);
->>>>>>> dbb6ac10ba5e620f54dd412835a038d38da01451
             this.label4.TabIndex = 25;
             this.label4.Text = "Fecha Vencimiento:";
             this.label4.Click += new System.EventHandler(this.label4_Click);
@@ -262,16 +213,9 @@
             this.dtp_Fecha_Vencimiento_Medicamentos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-<<<<<<< HEAD
-            this.dtp_Fecha_Vencimiento_Medicamentos.Location = new System.Drawing.Point(666, 182);
-            this.dtp_Fecha_Vencimiento_Medicamentos.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.dtp_Fecha_Vencimiento_Medicamentos.Name = "dtp_Fecha_Vencimiento_Medicamentos";
-            this.dtp_Fecha_Vencimiento_Medicamentos.Size = new System.Drawing.Size(448, 26);
-=======
             this.dtp_Fecha_Vencimiento_Medicamentos.Location = new System.Drawing.Point(545, 285);
             this.dtp_Fecha_Vencimiento_Medicamentos.Name = "dtp_Fecha_Vencimiento_Medicamentos";
             this.dtp_Fecha_Vencimiento_Medicamentos.Size = new System.Drawing.Size(277, 22);
->>>>>>> dbb6ac10ba5e620f54dd412835a038d38da01451
             this.dtp_Fecha_Vencimiento_Medicamentos.TabIndex = 26;
             this.dtp_Fecha_Vencimiento_Medicamentos.ValueChanged += new System.EventHandler(this.dtp_Fecha_Vencimiento_Medicamentos_ValueChanged);
             // 
@@ -283,17 +227,10 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.SystemColors.GrayText;
-<<<<<<< HEAD
-            this.label5.Location = new System.Drawing.Point(435, 230);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(104, 25);
-=======
             this.label5.Location = new System.Drawing.Point(842, 60);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(66, 20);
->>>>>>> dbb6ac10ba5e620f54dd412835a038d38da01451
             this.label5.TabIndex = 28;
             this.label5.Text = "Estado:";
             this.label5.Click += new System.EventHandler(this.label5_Click);
@@ -307,14 +244,6 @@
             this.cbx_Estado_Medicamentos.ForeColor = System.Drawing.SystemColors.GrayText;
             this.cbx_Estado_Medicamentos.FormattingEnabled = true;
             this.cbx_Estado_Medicamentos.Items.AddRange(new object[] {
-<<<<<<< HEAD
-            "Activo",
-            "Inactivo"});
-            this.cbx_Estado_Medicamentos.Location = new System.Drawing.Point(666, 230);
-            this.cbx_Estado_Medicamentos.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.cbx_Estado_Medicamentos.Name = "cbx_Estado_Medicamentos";
-            this.cbx_Estado_Medicamentos.Size = new System.Drawing.Size(448, 28);
-=======
             "Disponible",
             "Vencido",
             "Agotado",
@@ -324,7 +253,6 @@
             this.cbx_Estado_Medicamentos.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cbx_Estado_Medicamentos.Name = "cbx_Estado_Medicamentos";
             this.cbx_Estado_Medicamentos.Size = new System.Drawing.Size(277, 24);
->>>>>>> dbb6ac10ba5e620f54dd412835a038d38da01451
             this.cbx_Estado_Medicamentos.TabIndex = 27;
             // 
             // dgv_Medicamentos
@@ -334,20 +262,12 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dgv_Medicamentos.BackgroundColor = System.Drawing.SystemColors.ControlDarkDark;
             this.dgv_Medicamentos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-<<<<<<< HEAD
-            this.dgv_Medicamentos.Location = new System.Drawing.Point(389, 299);
-=======
             this.dgv_Medicamentos.Location = new System.Drawing.Point(346, 356);
->>>>>>> dbb6ac10ba5e620f54dd412835a038d38da01451
             this.dgv_Medicamentos.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dgv_Medicamentos.Name = "dgv_Medicamentos";
             this.dgv_Medicamentos.RowHeadersWidth = 51;
             this.dgv_Medicamentos.RowTemplate.Height = 24;
-<<<<<<< HEAD
-            this.dgv_Medicamentos.Size = new System.Drawing.Size(767, 312);
-=======
             this.dgv_Medicamentos.Size = new System.Drawing.Size(882, 288);
->>>>>>> dbb6ac10ba5e620f54dd412835a038d38da01451
             this.dgv_Medicamentos.TabIndex = 29;
             this.dgv_Medicamentos.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_Medicamentos_CellClick);
             // 
@@ -358,15 +278,9 @@
             this.iconPictureBox3.IconChar = FontAwesome.Sharp.IconChar.Pills;
             this.iconPictureBox3.IconColor = System.Drawing.SystemColors.ControlText;
             this.iconPictureBox3.IconFont = FontAwesome.Sharp.IconFont.Auto;
-<<<<<<< HEAD
-            this.iconPictureBox3.IconSize = 294;
-            this.iconPictureBox3.Location = new System.Drawing.Point(42, 156);
-            this.iconPictureBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-=======
             this.iconPictureBox3.IconSize = 244;
             this.iconPictureBox3.Location = new System.Drawing.Point(28, 239);
             this.iconPictureBox3.Margin = new System.Windows.Forms.Padding(4);
->>>>>>> dbb6ac10ba5e620f54dd412835a038d38da01451
             this.iconPictureBox3.Name = "iconPictureBox3";
             this.iconPictureBox3.Size = new System.Drawing.Size(294, 305);
             this.iconPictureBox3.TabIndex = 10;
@@ -382,11 +296,7 @@
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
-<<<<<<< HEAD
-            this.panel1.Size = new System.Drawing.Size(375, 625);
-=======
             this.panel1.Size = new System.Drawing.Size(333, 700);
->>>>>>> dbb6ac10ba5e620f54dd412835a038d38da01451
             this.panel1.TabIndex = 12;
             this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
             // 
@@ -686,9 +596,6 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
-<<<<<<< HEAD
-            this.ClientSize = new System.Drawing.Size(1170, 625);
-=======
             this.ClientSize = new System.Drawing.Size(1240, 700);
             this.Controls.Add(this.panel7);
             this.Controls.Add(this.panel6);
@@ -710,7 +617,6 @@
             this.Controls.Add(this.btn_Editar);
             this.Controls.Add(this.btn_Cancelar);
             this.Controls.Add(this.btn_Eliminar);
->>>>>>> dbb6ac10ba5e620f54dd412835a038d38da01451
             this.Controls.Add(this.dgv_Medicamentos);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.cbx_Estado_Medicamentos);
