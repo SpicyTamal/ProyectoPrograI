d80ce287b97408ef1fdf969b8cbdc451b3301b6d
Kevin <kevinobedmm@gmail.com>
2025-05-25 16:35:49 -0600

    Agregué la lógica del form Login

--- ProyectoPrograI-C#/ProyectoPrograI-C#/Seguridad/frm_Login.cs
+++ ProyectoPrograI-C#/ProyectoPrograI-C#/Seguridad/frm_Login.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using ProyectoPrograI_C_.Seguridad;
 
 namespace ProyectoProgra
 {
@@ -17,21 +19,43 @@ namespace ProyectoProgra
         {
             InitializeComponent();
         }
-        [DllImport("user32.Dll", EntryPoint = "ReleaseCapture")]
-        private extern static void ReleaseCapture();
-        [DllImport("user32.Dll", EntryPoint = "SendMessage")]
-        private extern static void SendMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        //METODOS Y LOGICA DEL FORM
+
+        private void msgError(string msg)
         {
-
+            lbl_Error.Text = "    " + msg;
+            lbl_Error.Visible = true;
         }
 
-        private void iconButton1_Click(object sender, EventArgs e)
+        private void btn_Acceder_Click(object sender, EventArgs e)
         {
-            this.WindowState = FormWindowState.Minimized;
+            if (txt_Usuario.Text != "USUARIO" && txt_Usuario.TextLength > 2)
+            {
+                if (txt_Contrasena.Text != "CONTRASEÑA")
+                {
+                    Modelo_Usuario user = new Modelo_Usuario();
+                    var validLogin = user.LoginUsuario(txt_Usuario.Text, txt_Contrasena.Text);
+                    if (validLogin == true)
+                    {
+                        //FrmMenuPrincipal menuPrincipal = new FrmMenuPrincipal(); Esperando a que Alex haga el form Munu
+                        MessageBox.Show("Bienvenido " + Cache_Usuario.Usuario + ", " + Cache_Usuario.TipoUsuario);
+                        //mainMenu.Show();
+                        //mainMenu.FormClosed += Logout;
+                        this.Hide();
+                    }
+                    else
+                    {
+                        msgError("Usuario o Contraseña incorrecta.");
+                        txt_Contrasena.Text = "Password";
+                        txt_Contrasena.UseSystemPasswordChar = false;
+                        txt_Usuario.Focus();
+                    }
+                }
+                else msgError("Please enter password.");
+            }
+            else msgError("Please enter username.");
         }
-
         private void txtUsuario_Enter(object sender, EventArgs e)
         {
             if (txt_Usuario.Text == "USUARIO")
@@ -48,11 +72,6 @@ namespace ProyectoProgra
                 txt_Usuario.Text = "USUARIO";
                 txt_Usuario.ForeColor = Color.DimGray;
             }
-        }
-
-        private void btnCerrar_Click(object sender, EventArgs e)
-        {
-            this.Close();
         }
 
         private void txtContrasena_Enter(object sender, EventArgs e)
@@ -76,6 +95,12 @@ namespace ProyectoProgra
             }
         }
 
+        //MOVIMIENTOS DEL FORM
+
+        [DllImport("user32.Dll", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.Dll", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
         private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
             ReleaseCapture();
@@ -88,7 +113,27 @@ namespace ProyectoProgra
             SendMessage(this.Handle, 0x112, 0xf012, 0);
         }
 
+
+        //BOTONES
+
+
+        private void iconButton1_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //OTROS
+
         private void frm_Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
