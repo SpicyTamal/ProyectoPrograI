6da11140fbc7e1b6bae0acbdfdc699965c556bb9
DylanMo2013 <127252979+DylanMo2013@users.noreply.github.com>
2025-05-25 23:13:09 -0600

    Se terminaron las funciones de agregar, eliminar, editar y cancelar de el forms habitaciones

--- ProyectoPrograI-C#/ProyectoPrograI-C#/Datos/CD_Habitaciones.cs
+++ ProyectoPrograI-C#/ProyectoPrograI-C#/Datos/CD_Habitaciones.cs
@@ -22,5 +22,46 @@ namespace ProyectoPrograI_C_.Datos
             return DT;
         }
 
+        public void Mtd_AgregarHabitaciones(string Numero, string Ubicacion, string TipoHabitacion, double Costo, string Estado, string UsuarioAuditoria, DateTime FechaAuditoria)
+        {
+            string QueryAgregarHabitaciones = "insert into tbl_Habitaciones(Numero,Ubicacion,TipoHabitaciones,Costo,Estado,UsuarioAuditoria,FechaAuditoria) values (@Numero,@Ubicacion,@TipoHabitaciones,@Costo,@Estado,@UsuarioAuditoria,@FechaAuditoria)";
+            SqlCommand cmd = new SqlCommand(QueryAgregarHabitaciones, CD_Conexion1.MtdAbrirConexion());
+            cmd.Parameters.AddWithValue("@Numero", Numero);
+            cmd.Parameters.AddWithValue("@Ubicacion", Ubicacion);
+            cmd.Parameters.AddWithValue("@TipoHabitaciones", TipoHabitacion);
+            cmd.Parameters.AddWithValue("@Costo", Costo);
+            cmd.Parameters.AddWithValue("@Estado", Estado);
+            cmd.Parameters.AddWithValue("@UsuarioAuditoria", UsuarioAuditoria);
+            cmd.Parameters.AddWithValue("@FechaAuditoria", FechaAuditoria);
+            cmd.ExecuteNonQuery();
+            CD_Conexion1.MtdCerrarConexion();
+        }
+
+        public void Mtd_ActualizarHabitaciones(int CodigoHabitacion, string Numero, string Ubicacion, string TipoHabitacion, double Costo, string Estado, string UsuarioAuditoria, DateTime FechaAuditoria)
+        {
+            string QueryActualizarHabitaciones = "update tbl_Habitaciones set Numero=@Numero, Ubicacion=@Ubicacion, TipoHabitaciones=@TipoHabitaciones, Costo=@Costo, Estado=@Estado, UsuarioAuditoria=@UsuarioAuditoria, FechaAuditoria=@FechaAuditoria where CodigoHabitacion=@CodigoHabitacion";
+
+            SqlCommand cmd = new SqlCommand(QueryActualizarHabitaciones, CD_Conexion1.MtdAbrirConexion());
+            cmd.Parameters.AddWithValue("@CodigoHabitacion", CodigoHabitacion);
+            cmd.Parameters.AddWithValue("@Numero", Numero);
+            cmd.Parameters.AddWithValue("@Ubicacion", Ubicacion);
+            cmd.Parameters.AddWithValue("@TipoHabitaciones", TipoHabitacion);
+            cmd.Parameters.AddWithValue("@Costo", Costo);
+            cmd.Parameters.AddWithValue("@Estado", Estado);
+            cmd.Parameters.AddWithValue("@UsuarioAuditoria", UsuarioAuditoria);
+            cmd.Parameters.AddWithValue("@FechaAuditoria", FechaAuditoria);
+            cmd.ExecuteNonQuery();
+            CD_Conexion1.MtdCerrarConexion();
+        }
+
+        public void Mtd_EliminarHabitaciones(int CodigoHabitacion)
+        {
+            string QueryEliminarHabitacion = "Delete tbl_Habitaciones where CodigoHabitacion=@CodigoHabitacion";
+            SqlCommand cmd = new SqlCommand(QueryEliminarHabitacion, CD_Conexion1.MtdAbrirConexion());
+            cmd.Parameters.AddWithValue("@CodigoHabitacion", CodigoHabitacion);
+            cmd.ExecuteNonQuery();
+            CD_Conexion1.MtdCerrarConexion();
+        }
+
     }
 }
--- ProyectoPrograI-C#/ProyectoPrograI-C#/Program.cs
+++ ProyectoPrograI-C#/ProyectoPrograI-C#/Program.cs
@@ -17,7 +17,7 @@ namespace ProyectoPrograI_C_
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frm_Medicamentos());
+            Application.Run(new frm_Habitaciones());
         }
     }
 }
