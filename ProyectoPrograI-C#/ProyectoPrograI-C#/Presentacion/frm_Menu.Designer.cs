8c64389454e49b590a7faba86c5ee8d9f9c4f8b3
alexcastle270 <bryancastle270@gmail.com>
2025-05-28 10:24:10 -0600

    progreso con el menu

---
+++ ProyectoPrograI-C#/ProyectoPrograI-C#/Presentacion/frm_Menu.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ProyectoPrograI_C_.Presentacion
+{
+    partial class frm_Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnl_Principal = new System.Windows.Forms.Panel();
+            this.pnl_Titulo = new System.Windows.Forms.Panel();
+            this.pnl_Menu = new System.Windows.Forms.Panel();
+            this.pnl_Formularios = new System.Windows.Forms.Panel();
+            this.pnl_Principal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnl_Principal
+            // 
+            this.pnl_Principal.Controls.Add(this.pnl_Formularios);
+            this.pnl_Principal.Controls.Add(this.pnl_Menu);
+            this.pnl_Principal.Controls.Add(this.pnl_Titulo);
+            this.pnl_Principal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnl_Principal.Location = new System.Drawing.Point(0, 0);
+            this.pnl_Principal.Name = "pnl_Principal";
+            this.pnl_Principal.Size = new System.Drawing.Size(1240, 700);
+            this.pnl_Principal.TabIndex = 2;
+            // 
+            // pnl_Titulo
+            // 
+            this.pnl_Titulo.BackColor = System.Drawing.Color.SaddleBrown;
+            this.pnl_Titulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnl_Titulo.Location = new System.Drawing.Point(0, 0);
+            this.pnl_Titulo.Name = "pnl_Titulo";
+            this.pnl_Titulo.Size = new System.Drawing.Size(1240, 40);
+            this.pnl_Titulo.TabIndex = 0;
+            // 
+            // pnl_Menu
+            // 
+            this.pnl_Menu.BackColor = System.Drawing.Color.Orange;
+            this.pnl_Menu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnl_Menu.Location = new System.Drawing.Point(0, 40);
+            this.pnl_Menu.Name = "pnl_Menu";
+            this.pnl_Menu.Size = new System.Drawing.Size(200, 660);
+            this.pnl_Menu.TabIndex = 1;
+            // 
+            // pnl_Formularios
+            // 
+            this.pnl_Formularios.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnl_Formularios.Location = new System.Drawing.Point(200, 40);
+            this.pnl_Formularios.Name = "pnl_Formularios";
+            this.pnl_Formularios.Size = new System.Drawing.Size(1040, 660);
+            this.pnl_Formularios.TabIndex = 2;
+            // 
+            // frm_Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1240, 700);
+            this.Controls.Add(this.pnl_Principal);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MinimumSize = new System.Drawing.Size(650, 650);
+            this.Name = "frm_Menu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Menu";
+            this.Load += new System.EventHandler(this.frm_Menu_Load);
+            this.pnl_Principal.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel pnl_Principal;
+        private System.Windows.Forms.Panel pnl_Titulo;
+        private System.Windows.Forms.Panel pnl_Menu;
+        private System.Windows.Forms.Panel pnl_Formularios;
+    }
+}
---
+++ ProyectoPrograI-C#/ProyectoPrograI-C#/Presentacion/frm_Menu.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoPrograI_C_.Presentacion
+{
+    public partial class frm_Menu : Form
+    {
+        public frm_Menu()
+        {
+            InitializeComponent();
+            //Estas lineas eliminan los parpadeos del formulario o controles en la interfaz grafica (Pero no en un 100%)
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+            this.DoubleBuffered = true;
+        }
+
+        private void btn_Cerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //RESIZE METODO PARA REDIMENCIONAR/CAMBIAR TAMAÑO A FORMULARIO EN TIEMPO DE EJECUCION ----------------------------------------------------------
+        private int tolerance = 12;
+        private const int WM_NCHITTEST = 132;
+        private const int HTBOTTOMRIGHT = 17;
+        private Rectangle sizeGripRectangle;
+
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case WM_NCHITTEST:
+                    base.WndProc(ref m);
+                    var hitPoint = this.PointToClient(new Point(m.LParam.ToInt32() & 0xffff, m.LParam.ToInt32() >> 16));
+                    if (sizeGripRectangle.Contains(hitPoint))
+                        m.Result = new IntPtr(HTBOTTOMRIGHT);
+                    break;
+                default:
+                    base.WndProc(ref m);
+                    break;
+            }
+        }
+        //----------------DIBUJAR RECTANGULO / EXCLUIR ESQUINA PANEL 
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            var region = new Region(new Rectangle(0, 0, this.ClientRectangle.Width, this.ClientRectangle.Height));
+
+            sizeGripRectangle = new Rectangle(this.ClientRectangle.Width - tolerance, this.ClientRectangle.Height - tolerance, tolerance, tolerance);
+
+            region.Exclude(sizeGripRectangle);
+            this.pnl_Principal.Region = region;
+            this.Invalidate();
+        }
+        //----------------COLOR Y GRIP DE RECTANGULO INFERIOR
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            SolidBrush blueBrush = new SolidBrush(Color.FromArgb(244, 244, 244));
+            e.Graphics.FillRectangle(blueBrush, sizeGripRectangle);
+
+            base.OnPaint(e);
+            ControlPaint.DrawSizeGrip(e.Graphics, Color.Transparent, sizeGripRectangle);
+        }
+
+        private void frm_Menu_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
