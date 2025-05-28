0f5e605792151c4ae2ec1847e3eb6bbc9870fe8c
Kevin <kevinobedmm@gmail.com>
2025-05-28 15:32:15 -0600

    Cambios por Kevin

---
+++ ProyectoPrograI-C#/ProyectoPrograI-C#/Logica/CL_Citas.cs
@@ -0,0 +1,32 @@
+﻿using ProyectoPrograI_C_.Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPrograI_C_.Logica
+{
+    internal class CL_Citas
+    {
+        CD_Citas conexion = new CD_Citas();
+
+        public double Mtd_CostoTratamiento(int CodigoCitas)
+        {
+            return conexion.Mtd_ConsultarCostoTratamiento(CodigoCitas);
+        }
+
+        public double Mtd_CostoHabitacion(int CodigoPaciente)
+        {
+            int CodigoHabitacion = conexion.Mtd_ConsultarCodigoHabitacion(CodigoPaciente);
+            double CostoHabitacion = conexion.Mtd_CostoHabitacion(CodigoHabitacion);
+            return CostoHabitacion;
+        }
+
+        public DateTime Mtd_FechaHoy()
+        {
+            return DateTime.Now;
+        }
+
+    }
+}
